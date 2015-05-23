a4eb554287ff5640966bdc99d43d38a2bfe79418
nashmaniac <nishantve1@gmail.com>
2015-05-23 15:13:40 +0530

    Added working sample chart

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+
+/*
+* Function : shadeColor(String value of hex color code, positive value to lighten negative to darken)
+* ---------------------------------------------------------------------------------------------------
+* Used to lighten or darken a hex color 
+*/
+function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+}
+
 // Creates and returns a new tagData object of format {tagName : , tagRating : }
 function newTagData(newTagName, newTagRating) {
 	return {
@@ -51,5 +78,73 @@ for(var i = 0 ; i < tagRatings.length ; i++) {
 
 // Rearranges the tag data array to ascending order by the tag rating 
 tagData.sort(descBy('tagRating'));
-console.table(tagData);
-console.log("This is something");
+
+///////////////
+
+// Converting tag data to DOM elements
+var tagContainer = document.querySelector('.tagContainer');
+tagData.forEach(function(currentTagData, index) {
+	var newTagDOMEl = document.createElement('div');
+	newTagDOMEl.style.backgroundColor = shadeColor('#5ECF81', index * 3);
+	newTagDOMEl.className = 'tag';
+	newTagDOMEl.textContent =  currentTagData['tagName'];
+	tagContainer.appendChild(newTagDOMEl);
+});
+
+
+/// For the Chart 
+
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+];
+
+var chartOptions = {
+    //Boolean - Whether we should show a stroke on each segment
+    segmentShowStroke : false,
+
+    //String - The colour of each segment stroke
+    segmentStrokeColor : "#fff",
+
+    //Number - The width of each segment stroke
+    segmentStrokeWidth : 3,
+
+    //Number - The percentage of the chart that we cut out of the middle
+    percentageInnerCutout : 50, // This is 0 for Pie charts
+
+    //Number - Amount of animation steps
+    animationSteps : 100,
+
+    //String - Animation easing effect
+    animationEasing : "easeOutQuint",
+
+    //Boolean - Whether we animate the rotation of the Doughnut
+    animateRotate : true,
+
+    //Boolean - Whether we animate scaling the Doughnut from the centre
+    animateScale : false,
+
+    //String - A legend template
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+
+};
+
+var chartView = document.getElementById("myChart");
+var ctx = chartView.getContext("2d");
+var myPieChart = new Chart(ctx).Pie(data,chartOptions);
